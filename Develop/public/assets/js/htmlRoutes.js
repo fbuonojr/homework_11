e5627d1cbefcbe11de243fd18177ecf428514c33
Frank Buono <fbuonojr@fordham.edu>
2020-09-09 15:27:23 -0400

    html route

--- Develop/public/assets/js/htmlRoutes.js
+++ Develop/public/assets/js/htmlRoutes.js
@@ -1,14 +1,7 @@
 var path = require("path");
 
-//look into router
 module.exports = function(app){
     app.get("/notes", function(req, res){
         res.sendFile(path.join(__dirname, "../../notes.html"));
-    })
-}
-
-module.exports = function(app){
-    app.get("*", function(req, res){
-        res.sendFile(path.join(__dirname, "../../index.html"));
     })
 }
