1d728c5599c104ed2ebb57c7ad0f6ae4bfe6aba7
Frank Buono <fbuonojr@fordham.edu>
2020-09-09 16:16:30 -0400

    added api delete route

--- Develop/public/assets/js/apiRoutes.js
+++ Develop/public/assets/js/apiRoutes.js
@@ -1,4 +1,5 @@
 var jsondb = require("../../../db/db.json");
+var fs = require("fs");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res){
@@ -8,6 +9,17 @@ module.exports = function(app) {
     app.post("/api/notes", function(req, res){
         var newNote = req.body;
         jsondb.push(newNote);
+        console.log(jsondb);
         res.json(newNote);
-    })
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+        const index = req.params.id;
+        jsondb.splice(index, 1);
+        fs.writeFile("../../../db/db.json", jsondb, function(err){
+            if(err){
+                console.log(err);
+            }
+        });
+    });
 }
