743696417b0780bbb0124e0957d681b9423086ec
Frank Buono <fbuonojr@fordham.edu>
2020-09-10 14:13:14 -0400

    fixed file structure

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./apiRoutes")(app);
-require("./htmlRoutes")(app);
+require("./public/assets/js/apiRoutes")(app);
+require("./public/assets/js/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: ", PORT);
